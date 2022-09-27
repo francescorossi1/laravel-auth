18005d029956e87ba0084c0a5d3ddc7f36996cc7
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-27 15:38:58 +0200

    added admin.posts.index link to admin.home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <a class="navbar-brand" href="{{ route('admin.posts.index') }}">
+                            Posts
+                        </a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
