392fbac7bf1c21046117d2294cc2b9d781b011c9
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-28 16:53:07 +0200

    created UserSeeder for testing, added category_id to seeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
@@ -14,9 +16,13 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+        $category_ids = Category::pluck('id')->toArray();
+
         for($i = 0; $i < 5; $i++){
             $post = new Post;
             $post->title = $faker->text(50);
+            $post->category_id = Arr::random($category_ids);
             $post->slug = Str::slug($post->title, '-');
             $post->content = $faker->paragraph(3, true);
             $post->image = $faker->imageUrl(200, 200);
