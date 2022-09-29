d502cd584b9fd7731520e17bb36e79073737db7c
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-29 17:09:51 +0200

    connected User and Post models

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Models\UserDetail');
     }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
 }
