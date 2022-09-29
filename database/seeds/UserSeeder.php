72d313094c4914fa43daaa2278fbca7386e2ecad
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-29 17:38:27 +0200

    updated UserSeeder for better testing

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Generator as Faker;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -10,12 +11,21 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         $user = new User();
         $user->name = 'Francesco';
         $user->email = 'francescorossi@boolean.it';
         $user->password = bcrypt('password');
         $user->save();
+
+        for($i = 0; $i < 4; $i++){
+            $user = new User();
+            $user->name = $faker->userName();
+            $user->email = $faker->email();
+            $user->password = bcrypt($faker->word());
+            $user->save();
+
+        }
     }
 }
